458b64efb17378f6f639d71303a9e4dc5896bcb0
ZhuYongjun <tmzyj2014@163.com>
2019-09-18 21:25:25 +0800

    update

--- src/main/java/com/huhupa/core/license/LicenseServiceImpl.java
+++ src/main/java/com/huhupa/core/license/LicenseServiceImpl.java
@@ -1,6 +1,10 @@
 package com.huhupa.core.license;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
 
 @Component("licenseService")
 public class LicenseServiceImpl implements LicenseService {
@@ -8,7 +12,16 @@ public class LicenseServiceImpl implements LicenseService {
 	@Override
 	public void generateKey() throws Exception {
 		// TODO Auto-generated method stub
-
+		License license = new License();
+		license.setExpiry(new Date(System.currentTimeMillis() + 60 * 60 * 24 * 1000));
+		license.setMacAddress("FE-FC-FE-F7-DB-65");
+		license.setModule(null);
+		license.setProductName("huhupa erp");
+		license.setSignature(null);
+		license.setType("trial");
+		license.setUserCountLimit(10);
+		license.setVersion("1.0");
+		
 	}
 
 	@Override
